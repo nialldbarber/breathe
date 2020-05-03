8a82324e2752655992c8705825f165654b0182bb
Niall Barber <niall.barber@actual-experience.com>
2020-05-03 10:18:47 +0100

    Updated divisor function tests

--- src/utils/time/__tests__/time.test.js
+++ src/utils/time/__tests__/time.test.js
@@ -1,6 +1,8 @@
-import { minToMs, secToMs, msToSec } from '~/utils/time';
+import { minToMs, secToMs, msToSec, getDivisors } from '~/utils/time';
 
 describe('Time utils', () => {
+  const expectedArr = [2, 3, 4, 5, 6, 10, 12, 15, 20, 30];
+
   test('1 minute equals 60000 milliseconds', () => {
     expect(minToMs(1)).toBe(60000);
   });
@@ -11,5 +13,13 @@ describe('Time utils', () => {
 
   test('1000 milliseconds to equal 1 seconds', () => {
     expect(msToSec(1000)).toBe(1);
+  });
+
+  test('Input of -1 should throw an error', () => {
+    expect(() => getDivisors(-1)).toThrow('Input must be more that 1');
+  });
+
+  test(`Input of 60 should yield ${expectedArr}`, () => {
+    expect(getDivisors(60)).toEqual(expectedArr);
   });
 });
